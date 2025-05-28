cc4fb9c2d1cb7a09dd96b5c4c6328c0d72f58f3f
Porkchawp <reubenwinfrey@gmail.com>
2025-05-28 18:07:24 -0400

    0.0.3: Rotate Object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init
@@ -7,12 +8,21 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.time.Clock()
+    dt = 0
+
+    yourShip = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2, PLAYER_RADIUS)
+
+    # GAME LOOP
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        yourShip.update(dt)
+        yourShip.draw(screen)
         pygame.display.flip()
+        dt = pygame.time.Clock().tick(60) / 1000
 
 if __name__ == "__main__":
     main()
